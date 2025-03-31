732ddee0cf6de06b3eb6f562908f7929c768661a
Gabriel Vechani <gabrielbrandaovechani@gmail.com>
2025-03-30 21:38:01 -0300

    Fix: ajuste no regex de nomes, para pegar mais de um nome

--- EP2.rb
+++ EP2.rb
@@ -1,4 +1,4 @@
-entrada = "Agendar com José reunião às 10:00 amanhã #trabalho, email: jose.da-silva@sp.senac.br, https://sp.senac.br/pag1#teste?aula=1&teste=4"
+entrada = "Agendar com José e João reunião às 10:00 amanhã #trabalho, email: jose.da-silva@sp.senac.br, https://sp.senac.br/pag1#teste?aula=1&teste=4"
 
 data = /([0-3]\d\sde\s[A-z]+(\sde\s\d{4})?)|([0-3]\d\/[0-3]\d(\/\d{4})?)|(hoje|amanhã|depois de amanhã)/.match(entrada)
 
@@ -10,14 +10,14 @@ url = /(https?:\/\/([A-Za-z0-9-]+\.?)+\.[a-z]+)(\/([a-z0-9-]+#)?([a-z0-9-]+\??)(
 
 email = /(\w+(\.|\-|\_)*)+\@([A-z]+\.)+([A-z])+/.match(entrada)
 
-nomes = /(?<=(e\s|\,\s))([ÍÉÂA-zãéêíóúü])+|(?<=(com\s))([ÍÉÂA-zãéêíóúü])+/.match(entrada)
+nomes = entrada.scan(/(?:com\s|e\s|para\s)([[:alpha:]]+)/i)
 
 verbos = /(([A-z])+r|reunião)(?=\s)/.match(entrada)
 
 saida = ""
 saida = !data.nil? ? "Data: #{data}\n" : ""
 saida += !horario.nil? ? "Horário: #{horario}\n" : ""
-saida += !nomes.nil? ? "Pessoa: #{nomes}\n" : ""
+saida += !nomes.nil? ? "Pessoa: #{nomes.flatten}\n" : ""
 saida += !email.nil? ? "Email: #{email}\n" : ""
 saida += !verbos.nil? ? "Ação: #{verbos}\n" : ""
 saida += !url.nil? ? "Url: #{url}\n" : ""
